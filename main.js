7ac1354506b6e46fbef1e6625fbd35c840575b39
Daniel Sikorski <93158258+sik0rski@users.noreply.github.com>
2023-02-20 14:49:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,79 @@
-let divSprawdz = document.getElementById("sprawdz");
-
-let sprawdzanieWyniku = () => {
-  console.log("sprawdzanie wyniku - start");
-  divSprawdz.innerHTML = `<button aria-busy="true">Prosimy poczekać…</button>`;
-  console.log("sprawdzanie wyniku - stop");
-};
-
-let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let checkboxes = document.querySelectorAll('input');
 
 console.log(checkboxes);
 
-let correctCheckboxes =
-  [
- 
+let correctCheckboxes = [
   checkboxes[0],
-  checkboxes[1], 
-  checkboxes[11], 
-  checkboxes[14], 
-  checkboxes[17], 
-  checkboxes[19], 
-  checkboxes[24], 
-  checkboxes[28], 
-  checkboxes[32],
-]
+  checkboxes[1],
+  checkboxes[7],
+  checkboxes[9],
+  checkboxes[12],
+  checkboxes[14],
+  checkboxes[19],
+  checkboxes[23],
+  checkboxes[27],
+  checkboxes[29],
+];
 
-// 4, 4, 4, 4, 2, 4, 2;
-// łącznie 24
+
+
+let points = 0;
+let countPoints = () => {
+  points = 0;
+  if (correctCheckboxes[0].checked == true) {
+    points++;
+    console.log(points);
+  }
+  if (correctCheckboxes[1].checked == true) {
+    points++;
+    console.log(points);
+  }
+  if (correctCheckboxes[2].checked == true) {
+    points++;
+    console.log(points);
+  }
+  if (correctCheckboxes[3].checked == true) {
+    points++;
+    console.log(points);
+  }
+  if (correctCheckboxes[4].checked == true) {
+    points++;
+    console.log(points);
+  }
+  if (correctCheckboxes[5].checked == true) {
+    points++;
+    console.log(points);
+  }
+  if (correctCheckboxes[6].checked == true) {
+    points++;
+    console.log(points);
+  }
+  if (correctCheckboxes[7].checked == true) {
+    points++;
+    console.log(points);
+  }
+  if (correctCheckboxes[8].checked == true) {
+    points++;
+    console.log(points);
+  }
+  if (correctCheckboxes[9].checked == true) {
+    points++;
+    console.log(points);
+  }
+  
+  
+
+    window.alert(`Twój wynik to: ${points}/10`);
+
+};
+
+let divSprawdz = document.getElementById("sprawdz");
+
+let sprawdzanieWyniku = () => {
+  let points = 0;
+  console.log("sprawdzanie wyniku - start");
+  divSprawdz.innerHTML = `<button aria-busy="true">Prosimy poczekać…</button>`;
+  countPoints();
+  console.log("sprawdzanie wyniku - stop");
+  divSprawdz.innerHTML = `<button onclick="sprawdzanieWyniku()">Sprawdź swój wynik</button>`;
+};
